d00e0f31aea6ad9fbce6df6e598a0f2964373440
flashybuild <jordi.or@gmail.com>
2021-03-10 21:04:58 +0100

    hello

--- src/App.js
+++ src/App.js
@@ -3,21 +3,15 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div 
+      style={{
+        margin: 'auto',
+        width: 150,
+        padding: '0.25rem',
+        border: '1px solid'
+      }}
+    >
+      <h1 className="title">Hello</h1>
     </div>
   );
 }
