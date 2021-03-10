82c074c7a23fb8af0b000fd9e2d050191d83d879
flashybuild <jordi.or@gmail.com>
2021-03-10 22:32:08 +0100

    pokemon 1st componet

--- src/App.js
+++ src/App.js
@@ -1,19 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import pokemons from './pokemons.json'
+
+const PokemonRow = ({pokemon}) => (
+  <tr>
+  <td>{pokemon.name.english}</td>
+  <td>{pokemon.name.japanese}</td>
+  <td>{pokemon.type.join(' & ')}</td>
+</tr>
+)
+
 
 function App() {
   return (
-    <div 
+    <div
       style={{
         margin: 'auto',
-        width: 150,
-        padding: '0.25rem',
-        border: '1px solid'
-      }}
-    >
-      <h1 className="title">Hello</h1>
+        marginLeft: '30px',
+        padding: '0.75rem',
+        paddingLeft: '1.2rem',
+        border: '1px solid',
+      }}>
+      <h1 className='title'>Hello</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>In japanese</th>
+            <th>Type</th>
+          </tr>
+        </thead>
+        <tbody>
+          {pokemons.slice(12, 20).map((pokemon) => (
+            <PokemonRow pokemon={pokemon} key={pokemon.id} />
+          ))}
+        </tbody>
+      </table>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
