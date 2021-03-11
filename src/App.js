9f374668c2b6beafc511a0d0168ef946953d3b58
flashybuild <jordi.or@gmail.com>
2021-03-11 23:04:31 +0100

    day 4 finished

--- src/App.js
+++ src/App.js
@@ -3,52 +3,64 @@ import './App.css'
 import pokemons from './pokemons.json'
 
 // function component
-const PokemonRow = ({ pokemon }) => (
+const PokemonRow = (props) => {
+  const { pokemonProp } = props
+return (
   <tr>
-    <td>{pokemon.name.english}</td>
-    <td>{pokemon.name.japanese}</td>
-    <td>{pokemon.type.join(' & ')}</td>
+    <td>{pokemonProp.name.english}</td>
+    <td>{pokemonProp.name.japanese}</td>
+    <td>{pokemonProp.type.join(' & ')}</td>
+    <td>
+      <button>Select</button>
+    </td>
   </tr>
-)
+)}
 
 function App() {
-  // state
-  const [filter, filterSet] = useState('')
+  // states
+  const [keyfilter, keyfilterSet] = useState('')
 
   return (
     <div
       style={{
         margin: 'auto',
-        marginLeft: '30px',
         padding: '0.75rem',
-        paddingLeft: '1.2rem',
         border: '1px solid',
       }}>
       <h1 className='title'>Search a pokemon</h1>
       <input
         type='text'
-        value={filter}
-        onChange={(event) => filterSet(event.target.value)}
+        value={keyfilter}
+        onChange={(event) => keyfilterSet(event.target.value)}
       />
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>In japanese</th>
-            <th>Type</th>
-          </tr>
-        </thead>
-        <tbody>
-          { pokemons
-            .filter((pokemon) => pokemon.name.english
-            .toLocaleLowerCase()
-            .includes(filter.toLocaleLowerCase()))
-            .slice(0, 30)
-            .map((pokemon) => (
-              <PokemonRow pokemon={pokemon} key={pokemon.id} />
-            ))}
-        </tbody>
-      </table>
+      <div
+        style={{
+          display: 'grid',
+          gridTemplateColumns: '70% 30%',
+          gridColumnGap: '1rem',
+        }}>
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>In japanese</th>
+              <th>Type</th>
+            </tr>
+          </thead>
+          <tbody>
+            {pokemons
+              .filter((pokemon) =>
+                pokemon.name.english
+                  .toLocaleLowerCase()
+                  .includes(keyfilter.toLocaleLowerCase()),
+              )
+              .slice(0, 30)
+              .map((pokemon) => (
+                <PokemonRow pokemonProp={pokemon} key={pokemon.id} />
+              ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   )
 }
